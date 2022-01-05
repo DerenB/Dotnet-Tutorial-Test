6b8a91ad3a1def7f33af54f88626ab50c0c833b1
Deren B <dbozerprogramming@gmail.com>
2022-01-05 01:16:51 -0500

    Test Form

--- ACCreatures/ACCreatures/Pages/Forms/AddAddress.cshtml.cs
+++ ACCreatures/ACCreatures/Pages/Forms/AddAddress.cshtml.cs
@@ -1,12 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ACCreatures.Models;
 
 namespace ACCreatures.Pages.Forms
 {
     public class AddAddressModel : PageModel
     {
+        [BindProperty]
+        public AddressModel Address { get; set; }
+
         public void OnGet()
         {
+
+        }
+
+        public IActionResult OnPost()
+        {
+            if(ModelState.IsValid == false)
+            {
+                return Page();
+            }
+
+            return RedirectToPage("./Index");
         }
     }
 }
