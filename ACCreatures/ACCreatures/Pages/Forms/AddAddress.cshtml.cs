8e0ad22569423ac3034067a9171ffc676dc908f7
Deren B <dbozerprogramming@gmail.com>
2022-01-04 21:33:20 -0500

    Create Test Page
    Creating a practice page called Add Address

---
+++ ACCreatures/ACCreatures/Pages/Forms/AddAddress.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ACCreatures.Pages.Forms
+{
+    public class AddAddressModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
